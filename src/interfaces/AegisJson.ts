051438a95539fc9dd323d89b0bcfcf2cd439975c
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-05-16 09:56:09 +0000

    Aegis JSON format interface

---
+++ src/interfaces/AegisJson.ts
@@ -0,0 +1,34 @@
+interface AegisJson{
+    "version": number;
+    "header": Header;
+    "db": Db;
+}
+
+interface Header{
+    "slots": null;
+    "params": null;
+}
+
+interface Db{
+    "version": number;
+    "entries": Array<Entry>;
+}
+
+interface Entry{
+    "type": string; // "totp", "steam"
+    "uuid": string;
+    "name": string; // service name
+    "issuer": string;
+    "note": string;
+    "favorite": boolean;
+    "icon": null;
+    "info": Info;
+    "groups": Array<string>; // this data type is probably wrong
+}
+
+interface Info{
+    "secret": string;
+    "algo": string; // eg. "SHA1"
+    "digits": number; // 6 for totp, 5 for steam,
+    "period": number; // usually 30
+}
