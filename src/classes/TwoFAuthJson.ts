51733b0fcd9d078198a35d23e353cbd7c8937e40
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-05-17 09:03:05 +0000

    Class for holding and converting 2FAuth JSON

---
+++ src/classes/TwoFAuthJson.ts
@@ -0,0 +1,35 @@
+import ITwoFAuthJson, { IToken } from "@/interfaces/ITwoFAuthJson";
+
+class TwoFAuthJson{
+
+    entries: Array<Token>;
+
+    constructor(str: string){
+        const json = JSON.parse(str) as ITwoFAuthJson;
+        this.entries = json.data.map(e => new Token(e));
+    }
+}
+
+class Token{
+    otp_type: string; // totp, steamtotp
+    account: string; //email
+    service: string | null; //service name
+    secret: string;
+    digits: number; //usually 6 for totp, 5 for steamotp
+    algorithm: string; //eg. sha1
+    period: number;
+    counter: null;
+    legacy_uri: string; //eg. otpauth://totp/blahblah
+
+    constructor(data: IToken){
+        this.otp_type = data.otp_type;
+        this.account = data.account;
+        this.service = data.service;
+        this.secret = data.secret;
+        this.digits = data.digits;
+        this.algorithm = data.algorithm;
+        this.period = data.period;
+        this.counter = data.counter;
+        this.legacy_uri = data.legacy_uri;
+    }
+}
